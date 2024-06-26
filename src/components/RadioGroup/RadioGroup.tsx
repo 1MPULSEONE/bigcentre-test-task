9f772c1d95853db3d8c19fa9157cf3ca8dd0ccec
Parkhomenko Egor <parkhomenko2049@gmail.com>
2024-06-27 03:04:40 +0600

    [Feat] Create RadioGroup component

---
+++ src/components/RadioGroup/RadioGroup.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import * as React from "react";
+import * as RadioGroupPrimitive from "@radix-ui/react-radio-group";
+import { cn } from "~/app/shared/lib";
+import { Circle } from "lucide-react";
+import Image from "next/image";
+
+const RadioGroup = React.forwardRef<
+  React.ElementRef<typeof RadioGroupPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof RadioGroupPrimitive.Root>
+>(({ className, ...props }, ref) => {
+  return (
+    <RadioGroupPrimitive.Root
+      className={cn("grid gap-2", className)}
+      {...props}
+      ref={ref}
+    />
+  );
+});
+RadioGroup.displayName = RadioGroupPrimitive.Root.displayName;
+
+const RadioGroupItem = React.forwardRef<
+  React.ElementRef<typeof RadioGroupPrimitive.Item>,
+  React.ComponentPropsWithoutRef<typeof RadioGroupPrimitive.Item>
+>(({ className, ...props }, ref) => {
+  return (
+    <RadioGroupPrimitive.Item
+      ref={ref}
+      className={cn(
+        "aspect-square h-4 w-4 items-center justify-center rounded-full border border-primary !p-0 text-primary focus:outline-none   disabled:cursor-not-allowed disabled:opacity-50",
+        className,
+      )}
+      {...props}
+    >
+      <RadioGroupPrimitive.Indicator className="flex items-center justify-center border-none">
+        <Image
+          width={16}
+          height={16}
+          alt={"radio button"}
+          src={"/icons/icon-radio-active.svg"}
+          className={"absolute z-10"}
+        />
+      </RadioGroupPrimitive.Indicator>
+    </RadioGroupPrimitive.Item>
+  );
+});
+RadioGroupItem.displayName = RadioGroupPrimitive.Item.displayName;
+
+export { RadioGroup, RadioGroupItem };
